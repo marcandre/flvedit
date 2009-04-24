4c439a4bf94c4f1b5cd878ecedb9a25339d239f0
Marc-Andre Lafortune <github@marc-andre.ca>
2009-04-24 18:20:40 -0400

    Minor change

--- lib/flv/edit/processor/save.rb
+++ lib/flv/edit/processor/save.rb
@@ -6,22 +6,17 @@ module FLV
       class Save < Base
         desc "Saves the result to PATH", :param => {:class => String, :name => "PATH"}
         
-        def each_source
-          return to_enum(:each_source) unless block_given?
-          @out = FLV::File::open(options[:save] || (h.path+".temp"), "w+b")
-          super
-        ensure
-          @out.close
-          finalpath = @out.path.sub(/\.temp$/, '')
-          FileUtils.mv(@out.path, finalpath) unless finalpath == @out.path
-        end
-        
         def each
           return to_enum unless block_given?
+          @out = FLV::File::open(options[:save] || (h.path+".temp"), "w+b")
           super do |chunk|
             @out << chunk
             yield chunk
           end
+        ensure
+          @out.close
+          finalpath = @out.path.sub(/\.temp$/, '')
+          FileUtils.mv(@out.path, finalpath) unless finalpath == @out.path          
         end
       end
     end
